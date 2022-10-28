c8255d1480fa710e303290a048e215ed7cad47be
Martin Nussbaum <maxvomrudel@gmail.com>
2022-10-29 00:16:16 +0200

    Letzter Stand vom Urlaub

--- src/main/java/de/martinnussbaum/newworldmod/Newworldmod.java
+++ src/main/java/de/martinnussbaum/newworldmod/Newworldmod.java
@@ -1,27 +1,30 @@
 package de.martinnussbaum.newworldmod;
 
 import com.mojang.logging.LogUtils;
-import de.martinnussbaum.newworldmod.core.init.ItemRegistry;
+import de.martinnussbaum.newworldmod.core.init.Blockinit;
+import de.martinnussbaum.newworldmod.core.init.ItemInit;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(NewWorldMod.MOD_ID)
-public class NewWorldMod {
 
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(newworldmod.MOD_ID)
+public class newworldmod {
     public static final String MOD_ID = "newworldmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public NewWorldMod() {
+    public newworldmod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ItemRegistry.register(modEventBus);
+        ItemInit.register(modEventBus);
+        Blockinit.register(modEventBus);
 
 
 
         MinecraftForge.EVENT_BUS.register(this);
     }
+
 }
--- src/main/java/de/martinnussbaum/newworldmod/core/init/ItemInit.java
+++ src/main/java/de/martinnussbaum/newworldmod/core/init/ItemInit.java
@@ -1,18 +1,19 @@
 package de.martinnussbaum.newworldmod.core.init;
 
+import de.martinnussbaum.newworldmod.world.item.ModCreativeModeTab;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import de.martinnussbaum.newworldmod.NewWorldMod;
+import de.martinnussbaum.newworldmod.newworldmod;
 
-public class ItemRegistry {
+public class ItemInit {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
-            ForgeRegistries.ITEMS, NewWorldMod.MOD_ID);
-    public static final RegistryObject<Item> TESTITEM = ITEMS.register("poralrahmen", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE)));
+            ForgeRegistries.ITEMS, newworldmod.MOD_ID);
+    public static final RegistryObject<Item> TESTITEM = ITEMS.register("poralrahmen", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.Tutorial_Tab)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
