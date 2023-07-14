0c6052dec8ae71b82a4a4679b307a7d4cec1d0aa
Martin Nussbaum <maxvomrudel@gmail.com>
2023-07-14 15:03:52 +0200

    beginning to add advanst items

---
+++ src/main/java/de/martinnussbaum/newworldmod/item/custom/DowsingRodItem.java
@@ -0,0 +1,60 @@
+package de.martinnussbaum.newworldmod.item.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import org.w3c.dom.Text;
+
+public class DowsingRodItem extends Item {
+    public DowsingRodItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if(pContext.getLevel().isClientSide()) {
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+
+            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
+                Block blockBelow = pContext.getLevel().getBlockState(positionClicked.below(i)).getBlock();
+
+                if(isValuableBlock(blockBelow)) {
+                    outputValuableCoordinates(positionClicked.below(i), player, blockBelow);
+                    foundBlock = true;
+                    break;
+                }
+            }
+
+            if(!foundBlock) {
+                player.sendSystemMessage(new Component ("item.newworldmod.dowsing_rod.no_valuables",
+                        player.getUUID()) {
+                });
+
+            }
+        }
+
+        pContext.getItemInHand().hurtAndBreak(1, pContext.getPlayer(),
+                (player) -> player.broadcastBreakEvent(player.getUsedItemHand()));
+
+        return super.useOn(pContext);
+    }
+
+
+    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block blockBelow) {
+        player.sendSystemMessage(new Text("Found " + blockBelow.asItem().getRegirstryName.toString() + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"), player.getUUID());
+    }
+
+    private boolean isValuableBlock(Block block) {
+        return block == Blocks.COAL_ORE || block == Blocks.COPPER_ORE
+                || block == Blocks.DIAMOND_ORE || block == Blocks.IRON_ORE;
+    }
+}
