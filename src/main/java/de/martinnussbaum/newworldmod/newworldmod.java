e9d87bd60b568bd1a537f9c97fbc663bd99f9310
Ralf Riedel <ralf.riedel@telekom.de>
2023-07-21 03:41:40 +0200

    implementation of buttons, trapdoors, pressureplates and doors, doors arent working

--- src/main/java/de/martinnussbaum/newworldmod/newworldmod.java
+++ src/main/java/de/martinnussbaum/newworldmod/newworldmod.java
@@ -3,9 +3,12 @@ package de.martinnussbaum.newworldmod;
 import com.mojang.logging.LogUtils;
 import de.martinnussbaum.newworldmod.block.ModBlocks;
 import de.martinnussbaum.newworldmod.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -23,6 +26,7 @@ public class newworldmod {
         ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
 
 
 
@@ -30,6 +34,11 @@ public class newworldmod {
     }
     private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("HELLO FROM PREINIT");
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_DOOR.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_TRAPDOOR.get(), RenderType.translucent());
     }
 
 }
